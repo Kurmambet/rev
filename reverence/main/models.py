f4360a1c3d893304e35421921700fc5da9caf2f3
Petr <pitr.vishnyakovv.05@gmail.com>
2025-06-27 19:53:58 +0500

    caatalog templates made

--- reverence/main/models.py
+++ reverence/main/models.py
@@ -2,8 +2,6 @@ from enum import unique
 from django.db import models
 
 
-
-
 # +----------------+        +----------------------+        +----------------+
 # |   Category     |◄───────┤    ClothingItem      ├───────┬►   Size         |
 # |----------------|        |----------------------|       |----------------|
@@ -33,7 +31,6 @@ from django.db import models
                         # | 3  | 1                  | 3        | True      |
 
 
-# Create your models here.
 class Size(models.Model):
     name = models.CharField(max_length=10, unique=True)
 
@@ -62,6 +59,7 @@ class ClothingItem(models.Model):
     sizes = models.ManyToManyField(Size, through='ClothingItemSize', 
                                    related_name='clothing_item', blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='clothing_items')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
     price = models.DecimalField(max_digits=20, decimal_places=2)
